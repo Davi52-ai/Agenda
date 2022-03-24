a2c434810a7acc25342e16d22f4eff49a310186f
Davi52-ai <davigedoz@gmail.com>
2022-03-24 16:06:47 -0300

    update 24/03

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -1,4 +1,4 @@
 class Kind < ApplicationRecord
     has_many :contact_kinds # possui muitos 
-    has_many :contacts, through: :contact_kinds #possui muitos contatos através de 
+    has_many :contacts, through: :contact_kinds #possui muitos contatos através de <tabela intermediaria>
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'contacts/show'
+  get 'contacts/new'
+  post 'contacts/create'
+  get 'contacts/edit'
+  put 'contacts/update'
+  get 'contacts/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
